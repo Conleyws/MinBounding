434c7271f387301bbcc734d3743ca2e3c62d87c0
Will Conley <conleyws@uwec.edu>
2021-09-02 19:18:35 -0600

    Deleted Coordinate/Area function - Finalized logic surrounding bounding

--- min-bounding/src/main/java/com/github/conleyws/GeoJSON.java
+++ min-bounding/src/main/java/com/github/conleyws/GeoJSON.java
@@ -1,33 +1,37 @@
 package com.github.conleyws;
 
 import java.util.List;
+import java.util.ArrayList;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 /**    
- *
- * POJO for JSON 
+ * POJO for GeoJSON
+ * Contains Type and Coordinates
  * 
  * @author wconley
+ * https://datatracker.ietf.org/doc/html/draft-ietf-geojson for more information
  */
 public class GeoJSON {
+    @JsonProperty("type")
     String type;
-    List<Coordinate> coordinates;
 
+    // I tried to parse these into a Coordinate Object with x/y but I could not get it to parse directly.
+    @JsonProperty("coordinates")
+    List<List<List<Double>>> coordinates;
 
-    @JsonCreator
-    public GeoJSON(@JsonProperty("type") String type, @JsonProperty("coordinates") List<Coordinate> coordinates) {
+    public GeoJSON() {
+        this.coordinates = new ArrayList<List<List<Double>>>();
+    }
+
+    public GeoJSON(String type, List<List<List<Double>>> coordinates) {
         this.type = type;
         this.coordinates = coordinates;
     }
 
-    @JsonGetter("coordinates")
-    public List<Coordinate> getCoordinates() {
+    public List<List<List<Double>>> getCoordinates() {
         return this.coordinates;
     }
 
-    @JsonGetter("type")
     public String getType() {
         return this.type;
     }
@@ -36,7 +40,7 @@ public class GeoJSON {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("Type: ").append(this.type);
-        builder.append("\nCoordinates: ").append(this.coordinates);
+        builder.append("| Coordinates: ").append(this.coordinates);
         return builder.toString();
     }
 
--- min-bounding/src/main/java/com/github/conleyws/service/PolygonService.java
+++ min-bounding/src/main/java/com/github/conleyws/service/PolygonService.java
@@ -1,68 +1,73 @@
 package com.github.conleyws.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import com.github.conleyws.Coordinate;
 import com.github.conleyws.GeoJSON;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Service to contain any logic surrounds Polygons
+ * 
+ */
 @Service
 public class PolygonService {
     
     /**
+     * Calculates the minimum bounds of passed in GeoJSON polygon
+     * and creates a new GeoJSON polygon using those bounds
      * 
-     * @param polygon
-     * @return GeoJSON - Bounding Shape
+     * @param polygon Shape which bounds are to be found
+     * @return Minimum Bounding Shape
+     * @see GeoJSON
      */
     public GeoJSON calculateMinBounding(GeoJSON polygon) {
-        // Must not be null and have at least two points
-        if (polygon == null || polygon.getCoordinates() == null || polygon.getCoordinates().size() < 3) {
+        if (polygon == null || polygon.getCoordinates() == null) {
             return polygon;
         }
         
         // 0 = minX
-        // 1 = maxY
-        // 2 = maxX
-        // 3 = minY 
+        // 1 = maxX
+        // 2 = minY 
+        // 3 = maxY
         Double[] bounds = new Double[4];
-
         // Traverse each coordinate and check if X/Y is new bound
-        for (Coordinate coord : polygon.getCoordinates()) {
-            // Min X
-            if (bounds[0] == null || coord.getX() < bounds[0]) {
-                bounds[0] = coord.getX();
-            }
-
-            // Max Y
-            if (bounds[1] == null || coord.getY() > bounds[1]) {
-                bounds[1] = coord.getY();
-            }
-
-            // Max X
-            if (bounds[2] == null || coord.getX() > bounds[2]) {
-                bounds[2] = coord.getX();
-            }
-
-            // Min Y
-            if (bounds[3] == null || coord.getY() < bounds[3]) {
-                bounds[3] = coord.getY();
+        for (List<List<Double>> shape : polygon.getCoordinates()){
+            for (List<Double> coord : shape) {
+                // Min X
+                if (bounds[0] == null || coord.get(0) < bounds[0]) {
+                    bounds[0] = coord.get(0);
+                }
+                // Max X
+                if (bounds[1] == null || coord.get(0) > bounds[1]) {
+                    bounds[1] = coord.get(0);
+                }
+                // Min Y
+                if (bounds[2] == null || coord.get(1) < bounds[2]) {
+                    bounds[2] = coord.get(1);
+                }
+                // Max Y
+                if (bounds[3] == null || coord.get(1) > bounds[3]) {
+                    bounds[3] = coord.get(1);
+                }
             }
         }
-
-        List<Coordinate> boundingCoords = new ArrayList<Coordinate>();
-        for (int i = 0; i < 5; i++) {
-            boundingCoords.add(new Coordinate(bounds[i % 4], bounds[(i+1) % 4]));
-        }
-        // Assuming starting at Top Left and continuing clockwise
+        
+        // Starting at Top Left and continuing clockwise
+        List<List<Double>> boundingCoords = new ArrayList<List<Double>>();
         // MinX MaxY
+        boundingCoords.add(Arrays.asList(bounds[0], bounds[3]));
         // MaxX MaxY
+        boundingCoords.add(Arrays.asList(bounds[1], bounds[3]));
         // MaxX MinY
+        boundingCoords.add(Arrays.asList(bounds[1], bounds[2]));
         // MinX MinY
+        boundingCoords.add(Arrays.asList(bounds[0], bounds[2]));
         // MinX MaxY - Have to return to start
-
-
-        return new GeoJSON(polygon.getType(), boundingCoords);
+        boundingCoords.add(Arrays.asList(bounds[0], bounds[3]));
+        
+        return new GeoJSON(polygon.getType(), new ArrayList<>(Arrays.asList(boundingCoords)));
     }
 }
